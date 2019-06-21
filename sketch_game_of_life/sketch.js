6c04c23b2770888196b709f0ecdab108f8724ed4
t0mm4rx <tom.marx@hotmail.fr>
2019-06-21 22:24:13 +0200

    Colors

--- sketch_game_of_life/sketch.js
+++ sketch_game_of_life/sketch.js
@@ -6,12 +6,7 @@ function setup() {
 	createCanvas(800, 800);
 
 	grid = get_empty_grid();
-
-	grid[50][50] = 1
-	grid[50][51] = 1
-	grid[50][52] = 1
-	//grid[49][51] = 1
-	for (var i = 0; i < 1000; i++) {
+	for (var i = 0; i < 2000; i++) {
 		grid[Math.floor(Math.random() * GRID_SIZE)][Math.floor(Math.random() * GRID_SIZE)] = 1;
 	}
 
@@ -21,12 +16,13 @@ function draw() {
 	background(50, 50, 70);
 
 	/* Drawing grid */
-	fill(230);
 	stroke(50, 50, 70);
 	for (var x = 0; x < GRID_SIZE; x++) {
 		for (var y = 0; y < GRID_SIZE; y++) {
-			if (grid[x][y] == 1)
+			if (grid[x][y] != 0) {
+				fill(255 - grid[x][y] ** 2 * 10, 70, max(grid[x][y] ** 2 * 10, 210));
 				rect(x * PIXEL_SIZE, y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+			}
 		}
 	}
 
@@ -35,11 +31,12 @@ function draw() {
 		for (var y = 0; y < GRID_SIZE; y++) {
 			n = get_neighbours(x, y);
 
-			if (grid[x][y] == 1) {
+			if (grid[x][y] != 0) {
 				if (n != 2 && n != 3) {
 					ngrid[x][y] = 0;
 				} else {
-					ngrid[x][y] = 1;
+					ngrid[x][y] = grid[x][y] + 1;
+					//ngrid[x][y] = 1;
 				}
 			} else {
 				if (n == 3) {
@@ -68,14 +65,14 @@ function get_empty_grid() {
 
 function get_neighbours(x, y) {
 	let s = 0;
-	s += (grid[safe_coords(x - 1)][safe_coords(y)] == 1)
-	s += (grid[safe_coords(x + 1)][safe_coords(y)] == 1)
-	s += (grid[safe_coords(x + 1)][safe_coords(y + 1)] == 1)
-	s += (grid[safe_coords(x - 1)][safe_coords(y + 1)] == 1)
-	s += (grid[safe_coords(x - 1)][safe_coords(y - 1)] == 1)
-	s += (grid[safe_coords(x + 1)][safe_coords(y - 1)] == 1)
-	s += (grid[safe_coords(x)][safe_coords(y - 1)] == 1)
-	s += (grid[safe_coords(x)][safe_coords(y + 1)] == 1)
+	s += (grid[safe_coords(x - 1)][safe_coords(y)] != 0)
+	s += (grid[safe_coords(x + 1)][safe_coords(y)] != 0)
+	s += (grid[safe_coords(x + 1)][safe_coords(y + 1)] != 0)
+	s += (grid[safe_coords(x - 1)][safe_coords(y + 1)] != 0)
+	s += (grid[safe_coords(x - 1)][safe_coords(y - 1)] != 0)
+	s += (grid[safe_coords(x + 1)][safe_coords(y - 1)] != 0)
+	s += (grid[safe_coords(x)][safe_coords(y - 1)] != 0)
+	s += (grid[safe_coords(x)][safe_coords(y + 1)] != 0)
 	return s
 }
 
