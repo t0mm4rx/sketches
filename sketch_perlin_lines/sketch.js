c8e22377ad7f90a78a5e4842d7ad67495cc4aabc
t0mm4rx <tom.marx@hotmail.fr>
2019-10-01 01:18:27 +0200

    Perlin noise 1

---
+++ sketch_perlin_lines/sketch.js
@@ -0,0 +1,31 @@
+const LINES = 6;
+const NOISE_REDUCTION = 0.02;
+const AMPLITUDE = 100;
+const MARGIN = 50;
+const MARGIN_START = 260;
+
+function setup() {
+	createCanvas(800, 800);
+}
+
+function draw() {
+	background(0);
+	strokeWeight(3);
+	stroke(255);
+
+	for (let i = 0; i < LINES; i++)
+	{
+		let y = MARGIN_START + i * MARGIN;
+		for (let x = 0; x < width; x++)
+		{
+			let d = noise((x + mouseX * 1.5 * (i + 1)) * NOISE_REDUCTION, (i * 100 * NOISE_REDUCTION)) * AMPLITUDE;
+			stroke((200 / LINES) * (i + 1));
+			line(
+				x,
+				y + d,
+				x + 1,
+				height
+			);
+		}
+	}
+}
