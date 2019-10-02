e00735e4d4be26146138a9ca210823484b4408fd
t0mm4rx <tom.marx@hotmail.fr>
2019-10-02 16:01:59 +0200

    style black and white

--- new.sh
+++ new.sh
@@ -8,7 +8,7 @@ fi
 mkdir sketch_$1 && cd sketch_$1
 
 echo -e "<html>\n<head>\n\t<title>Sketch $1</title>\n\t<meta charset=\"UTF-8\">\n\t<style>body, html { margin: 0; padding: 0; width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; flex-direction: column;}</style>\n</head>\n\n<body></body>\n\n<script src=\"../p5.min.js\"></script>\n<script src=\"../interpolation.min.js\"></script>\n<script src=\"sketch.js\"></script>\n</html>" > index.html
-echo -e "function setup() {\n\tcreateCanvas(800, 800);\n}\n\nfunction draw() {\n\tbackground(50, 50, 70);\n}" > sketch.js
+echo -e "var font;\n\nfunction preload() {\n\tfont = loadFont('../fonts/PlayfairDisplay-Regular.ttf');\n}\n\nfunction setup() {\n\tcreateCanvas(800, 800);\n}\n\nfunction draw() {\n\tbackground(0);\n}" > sketch.js
 
 cd ../
 echo -e "* [$1](https://t0mm4rx.github.io/sketches/sketch_$1/)" >> ReadMe.md
--- sketch_pi_sequence/sketch.js
+++ sketch_pi_sequence/sketch.js
@@ -3,13 +3,20 @@ var points = [null, null, null, null, null, null, null, null, null, null];
 var current = 0;
 var graph;
 var graph2;
+var font;
+
+function preload() {
+	font = loadFont("../fonts/PlayfairDisplay-Bold.ttf");
+}
 
 function setup() {
 	frameRate(4);
 	createCanvas(800, 800);
 	graph = createGraphics(800, 800);
 	graph2 = createGraphics(800, 800);
-	graph2.background(50, 50, 70);
+	graph.textFont(font);
+	graph2.textFont(font);
+	graph2.background(0);
 	angleMode(RADIANS);
 	graph2.stroke(255);
 	graph2.noFill();
@@ -24,12 +31,14 @@ function setup() {
 		graph2.ellipse(width / 2 + vector.x, height / 2 + vector.y, 3, 3);
 		graph2.textSize(30);
 		graph2.textAlign(CENTER, CENTER);
+		graph2.noStroke();
+		graph2.fill(255);
 		graph2.text((i + 1) % 10, width / 2 + vector.x * 1.08, height / 2 + vector.y * 1.08);
 	}
 }
 
 function draw() {
-	background(50, 50, 70);
+	background(0);
 	graph2.strokeWeight(1);
 	graph2.stroke(255, 255, 255, 20);
 	graph2.line(points[parseInt(pi_digits[current])].x + width / 2, points[parseInt(pi_digits[current])].y + height / 2, points[parseInt(pi_digits[current + 1])].x + width / 2, points[parseInt(pi_digits[current + 1])].y + height / 2);
@@ -37,6 +46,20 @@ function draw() {
 	graph.stroke(200, 100, 100);
 	graph.strokeWeight(2);
 	graph.line(points[parseInt(pi_digits[current])].x + width / 2, points[parseInt(pi_digits[current])].y + height / 2, points[parseInt(pi_digits[current + 1])].x + width / 2, points[parseInt(pi_digits[current + 1])].y + height / 2);
+
+	graph.textAlign(CENTER, CENTER);
+	graph.fill(255);
+	graph.noStroke();
+	graph.textSize(50);
+	let t = "3.14";
+	if (current > 2) {
+		t += "..";
+	}
+	if (current >= 2) {
+		t += pi_digits[current] + pi_digits[current + 1];
+	}
+	graph.text(t, width / 2, height / 2);
+
 	image(graph2, 0, 0);
 	image(graph, 0, 0);
 	graph.clear();
